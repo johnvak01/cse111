a76874c84b40e35d6d9ba7fc97680f3389999f59
johnvak01 <johnvak01@gmail.com>
2025-10-13 21:27:55 -0600

    added basics of prosody analysis

--- project_final/prosody_analyzer.py
+++ project_final/prosody_analyzer.py
@@ -5,7 +5,7 @@ Week #
 Assignment Name
 '''
 
-import requests,json
+import requests,json,string
 
 
 def main():
@@ -15,11 +15,15 @@ def main():
     RETURN: 0
     '''
     # get desired text
-    text = input("What text are we analyzing?")
-    # clean the text
-    text = clean_text(text)
-    analysis = get_api_response(text)
-
+    text = input("The Text to analyze: ")
+    
+    cleaned_text = clean_text(text)
+    print(cleaned_text)
+    text_analysis = get_api_response(cleaned_text)
+    print(text_analysis)
+    text_stresses = calculate_stresses(text_analysis)
+    foot_pattern = calculate_foot_pattern(text_stresses)
+    print(foot_pattern)
     return 0
 
 # Helper Functions
@@ -30,10 +34,9 @@ def clean_text(text):
     PARAM:  text
     RETURN: list of words
     '''
-    cleaned_text = text.split()
-    print(cleaned_text)
+    cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
+    cleaned_text = cleaned_text.lower().split()
     return cleaned_text
-
 
 def get_api_response(text):
     '''
@@ -43,28 +46,176 @@ def get_api_response(text):
     '''
     responses = []
     for word in text:
-        raw_request = requests.get(f"https://api.datamuse.com/words?sp={word}&max=1&md=r").text
-        print(raw_request)
+        raw_request = requests.get(f"https://api.datamuse.com/words?sp={word}&max=1&md=r&ipa=1").text
         raw_request = json.loads(raw_request)
-        print(raw_request)
         responses.append([raw_request[0]["word"],raw_request[0]["tags"][0]])
-    print(responses)
     return responses
 
-def calculate_meter():
+def calculate_stresses(text):
     '''
     FOR:    take the analysed text and calculatre the meter
-    PARAM:  x
-    RETURN: x
+    PARAM:  text
+    RETURN: string of 1's and 0's indicating the stresses
     '''
-    pass
-def match_metrical_pattern():
+    stresses = ""
+    for word in text:
+        for character in word[1]:
+            if character in string.digits:
+                stresses+=character
+    print(stresses)
+    return stresses
+
+def check_syllables(pattern, check_list, final_list):
+    '''
+    FOR:    checking a pattern vs entries in a list of syllables  
+    PARAM:  the pattern to check, the list of patterns to check it against, the final list to be modified by a matched pattern
+    RETURN: how many lines to skip, or 0 if no match was found
+    '''
+    for syllable in check_list:
+        if pattern == syllable[0]:
+           final_list.append(syllable[1])
+           return len(syllable[0])-1
+    return 0
+
+
+def calculate_foot_pattern(pattern):
+    '''
+    FOR:    determine if a metrical pattern matches a particular poems pattern  
+    PARAM:  the pattern to check as a string of 1's and 0's, 1's being stressed syllables and 0's being unstressed
+    RETURN: a list of the poetic feet for the pattern
+    '''
+    # feet patterns: "01"-"iamb", "10"-"trochee", "110"-"anapest", "011"-"dactyl", 
+    # only top be found in the revision "101" - amphibrach, "100" - bacchius, "010" - cretic, 011 - dactyl
+
+    # list the syllables we are going to be testig against
+    disyllables = [
+                    ["00","dibrach"],
+                    ["01","iamb"],
+                    ["10","trochee"],
+                    ["11","spondee"],
+                    ]
+    trisyllables = [
+                    ["000","tribrach"],
+                    ["100","dactyl"],
+                    ["010","amphibrach"],
+                    ["001","anapest"],
+                    ["011","bacchius"],
+                    ["101","amphimacer"],
+                    ["110","antibacchius"],
+                    ["111","molossus"]
+                    ]
+    disyllables_common = [
+                    ["01","iamb"],
+                    ["11","spondee"]
+                    ]   
+    trisyllables_common = [                
+                    ["001","anapest"],
+                    ["100","dactyl"]
+                    ]
+    disyllables_uncommon = [
+                    ["10","trochee"],
+                    ["00","pyrrhic"]
+                    ]
+    trisyllables_uncommon = [
+                    ["110","antibacchius"],
+                    ["010","amphibrach"],
+                    ["000","tribrach"],
+                    ["111","molossus"],
+                    ["011","bacchius"],
+                    ["101","cretic"]
+                    ]
+    unisyllables = [
+                    ["0", "macer"],
+                    ["1", "brach"]
+                    ]
+    
+    # do initial setup   
+    foot_pattern = []
+    pattern_length = len(pattern)
+    skip_step = 0
+
+    for index in range(0, pattern_length):
+        # ship this round if previous checks indicate so
+        if skip_step > 0:
+            skip_step -= 1
+            continue
+        # establish what we're testing
+        pattern_remaining = pattern_length - index
+        print(pattern_remaining)
+        unisyllable = pattern[index]
+        disyllable = pattern[index]+pattern[index+1]
+        if pattern_remaining > 2:
+            trisyllable = pattern[index]+pattern[index+1]+pattern[index+2]
+        else:
+            trisyllable = ""
+        
+        # start testing
+        if pattern_remaining > 4:
+            # check most likely
+            if skip_step == 0:
+                skip_step = check_syllables(disyllable, disyllables_common, foot_pattern)
+            if skip_step == 0:
+                skip_step = check_syllables(trisyllable, trisyllables_common, foot_pattern)
+            if skip_step == 0:
+                skip_step = check_syllables(disyllable, disyllables_uncommon, foot_pattern)
+            if skip_step == 0:
+                skip_step = check_syllables(trisyllable, trisyllables_uncommon, foot_pattern)
+            if skip_step == 0:
+                skip_step = check_syllables(unisyllable, unisyllables, foot_pattern)
+            continue
+        elif pattern_remaining == 3:
+            skip_step = check_syllables(trisyllable, trisyllables, foot_pattern)
+            if skip_step == 0:
+                skip_step = check_syllables(trisyllable, trisyllables_uncommon, foot_pattern)
+            continue
+        elif pattern_remaining == 2 or pattern_remaining == 4: 
+            skip_step = check_syllables(disyllable, disyllables, foot_pattern)
+            continue
+    return foot_pattern
+def match_metrical_pattern(passed_pattern):
     '''
     FOR:    determine if a metrical pattern matches a particular poems pattern  
     PARAM:  x
     RETURN: x
     '''
-    pass
+    number_of_feet = ["monometer", "dimeter", "trimeter", "tetrameter", "pentameter", "hexameter", "heptameter", "octameter", "nonameter"]
+    mono_foot_types = [
+        ["dibrach", "dibrachic"],
+        ["iamb", "iambic"],
+        ["trochee", "trocheeic"],
+        ["spondee", "spondeeic"],
+        ["tribach", "tribrachic"],
+        ["dactyl", "dactylic"],
+        ["amphibrach", "amphibrachic"],
+        ["anapest", "anapestic"],
+        ["bacchius", "bacchiusic"],
+        ["amphimacer", "amphimaceric"],
+        ["antibacchius", "antibacchiusic"],
+        ["molossus", "molossusic"]
+        ]
+    # patterns = ["iambic pentameter", "alexandrine", "blank verse", "Common Meter", "long Meter"]
+
+    output = ""
+
+    # calculate mono_pattern
+    mono = True
+    for type in mono_foot_types:
+        for foot in passed_pattern:
+            if type[0] != foot:
+                mono = False
+        if mono == True:
+            output+=type[1]+" "
+            break
+        else:
+            mono = True
+    
+    # calculate total number of feet in line
+    total_feet = len(passed_pattern)
+    output+=number_of_feet[total_feet-1]
+    # return output
+    return output
+
+# Extras
 def check_alliteration():
     '''
     FOR:    check for matching consonant sounds
@@ -79,7 +230,6 @@ def check_rhyme():
     RETURN: x
     '''
     pass
-
 def lineate_text():
     '''
     FOR:    take a text and convert it into poetic form, seperating lines etc.
@@ -87,7 +237,7 @@ def lineate_text():
     RETURN: x
     '''
     pass
-    
+
 # Main call
 if __name__ == "__main__":
     main()
---
+++ project_final/prosody_analyzer_test.py
@@ -0,0 +1,55 @@
+'''
+J Austin Hutchinson
+CSE 111
+Week #
+Assignment Name
+'''
+from pytest import approx
+import pytest
+import random
+
+# import functions to test
+from prosody_analyzer import calculate_stresses, calculate_foot_pattern, match_metrical_pattern, check_alliteration, check_rhyme, lineate_text
+
+def test_basic_foot_pattern():
+    '''
+    FOR:    x 
+    PARAM:  x
+    RETURN: x
+    '''
+    # text basic feet
+    assert calculate_foot_pattern("00") == ["dibrach"]
+    assert calculate_foot_pattern("01") == ["iamb"]
+    assert calculate_foot_pattern("10") == ["trochee"]
+    assert calculate_foot_pattern("11") == ["spondee"]
+    assert calculate_foot_pattern("001") == ["anapest"]
+    assert calculate_foot_pattern("100") == ["dactyl"]
+
+def test_joined_foot_patterns():
+    # test doubles
+    assert calculate_foot_pattern("0000") == ["dibrach","dibrach"]
+    assert calculate_foot_pattern("0101") == ["iamb","iamb"]
+    assert calculate_foot_pattern("1010") == ["trochee","trochee"]
+    assert calculate_foot_pattern("1111") == ["spondee","spondee"]
+    assert calculate_foot_pattern("001001") == ["anapest","anapest"]
+    assert calculate_foot_pattern("100100") == ["dactyl","dactyl"]
+    
+    # test permutations of two
+
+    # assert calculate_foot_pattern("0000") == [" py"," "]
+
+def test_random_patterns():
+    try:
+        for x in range(20):
+            pattern = "".join(random.choices(["0","1"], k=random.randint(0,100)))
+            print(pattern)
+            assert calculate_foot_pattern(pattern)
+    except:
+        pytest.fail("Random Patterns failed to produce coherent results")
+
+def test_foot_pattern_matching():
+    assert match_metrical_pattern(["iamb"]) == "iambic monometer"
+    assert match_metrical_pattern(["iamb","dactyle"]) == "dimeter"
+    assert match_metrical_pattern(["iamb","iamb","iamb","iamb","iamb"]) == "iambic pentameter"
+# Main call
+pytest.main(["-v", "--tb=line", "-rN", __file__])
